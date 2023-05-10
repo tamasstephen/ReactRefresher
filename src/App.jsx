69aed86ff3eb944ff2479408aa469e39ec74566e
tamasstephen <kicsitnagyon@gmail.com>
2023-05-10 13:56:33 +0200

    feat: Add react-hooks plugin to eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import { createRoot } from "react-dom";
-import Pet from "./Pet.jsx";
+import { createRoot } from "react-dom/client";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese" />
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
-      <Pet name="Apollo" animal="Cat" breed="Persian" />
+      <SearchParams />
     </div>
   );
 };
