47895f23a7dcc285b7479575a2b0a6f6b3e9d6c6
tamasstephen <kicsitnagyon@gmail.com>
2023-05-10 21:14:43 +0200

    feat: Update react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { createRoot } from "react-dom/client";
 import SearchParams from "./SearchParams";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Details from "./Details";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Adopt me</h1>
-      <SearchParams />
-    </div>
-  );
-};
+const router = createBrowserRouter([
+  {
+    element: (
+      <>
+        <h1>Adopt me!</h1>
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <SearchParams />,
+      },
+      {
+        path: "/details/:id",
+        element: <Details />,
+      },
+    ],
+  },
+]);
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(<App />);
+root.render(<RouterProvider router={router} />);
