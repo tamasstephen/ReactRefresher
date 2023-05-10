4261d38f7723a24aec0a66249ad6a18e8ca9e682
tamasstephen <kicsitnagyon@gmail.com>
2023-05-10 15:14:48 +0200

    feat: Add fields to search form

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,7 +1,12 @@
 import { useState } from "react";
 
+const ANIMALS = ["", "bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
 
   return (
     <div className="search-params">
@@ -14,6 +19,40 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={(e) => setLocation(e.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            name=""
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            {ANIMALS.map((currentAnimal, idx) => (
+              <option value={currentAnimal} key={idx}>
+                {currentAnimal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            name=""
+            id="breed"
+            disabled={!breeds.length}
+            value={breed}
+            onChange={(e) => setBreed(e.target.value)}
+          >
+            {breeds.map((currentBreed, idx) => (
+              <option value={currentBreed} key={idx}>
+                {currentBreed}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
